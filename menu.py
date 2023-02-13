26376bac47ceef423b4d1bf54304be6cdfaf0b12
Swarnadeep Seth <65512811+SwarnadeepSeth@users.noreply.github.com>
2023-02-13 18:29:53 -0500

    Add files via upload

---
+++ menu.py
@@ -0,0 +1,25 @@
+### Python code to place menubar in the respective html files by reading the menu.html file ###
+
+filename_list=["index.html", "contact.html", "market-overview.html", "market-valuation_Example.html", 
+"market-valuation_IND.html", "market-valuation_US.html", "datascience_QA.html", "publications.html", "research-post.html"]
+
+for filename in filename_list:
+
+    print ("FileName is:", filename)
+
+    with open(filename) as f:
+        contents = f.read()
+
+    with open("menu.html") as f:
+        menu_contents = f.read()
+
+    start_marker = "<!-- Navigation-->"
+    end_marker = "<!-- Page Content-->"
+
+    start_index = contents.index(start_marker) + len(start_marker)
+    end_index = contents.index(end_marker)
+
+    contents = contents[:start_index] + "\n" + menu_contents + "\n" + contents[end_index:]
+
+    with open(filename, "w") as f:
+        f.write(contents)
